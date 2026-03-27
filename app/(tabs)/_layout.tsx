d1ee31c415957401e496b6a0a4fa2e2c614b3741
Luisa Santana <igor.g.alves7@aluno.senai.br>
2026-03-27 17:52:38 -0300

    .

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { Stack } from "expo-router";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!);
+
+export default function RootLayout() {
+  return (
+    <ConvexProvider client={convex}>
+      <Stack />
+    </ConvexProvider>
+  );
+}
